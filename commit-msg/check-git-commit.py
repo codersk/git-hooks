5fdb5d6c9325102689d5a6b2a0a6b6a7b1d687e1
codersk <sandeepkumbhar007@gmail.com>
2019-07-18 16:32:10 +0530

    check format rules

--- commit-msg/check-git-commit.py
+++ commit-msg/check-git-commit.py
@@ -1,8 +1,64 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import re
 
 from subprocess import call
+
+if "EDITOR" in os.environ.keys():
+    editor = os.environ["EDITOR"]
+else:
+    editor = "/usr/bin/nano"
+
+#Regex patterns
+regex1 = r"^\w+"
+regex2 = r"^(feat|fix|docs|style|refactor|test|chore)"
+
+def check_format_rules(lineno, line):
+    real_lineno = lineno + 1
+    if lineno == 0:
+        if len(line) > 50:
+            return "E%d: First line is the HEADER.\n " \
+                " - HEADER is a single line of max. 50 characters that contains a \n" \
+                "  small description of the change. It contains a type, an optional \n" \
+                "  scope and a subject\n" \
+                "  + <type> describes the kind of change that this commit is providing. \n" \
+                "    Allowed types are:\n" \
+                "        * feat      - (feature)\n" \
+                "        * fix       - (bug fix)\n" \
+                "        * docs      - (documentation)\n" \
+                "        * style     - (formatting, missing semi colons,...)\n" \
+                "        * refactor  - (refactor)\n" \
+                "        * test      - (when adding missing tests)\n" \
+                "        * chore     - (maintain)\n" \
+                "  + <scope> can be anything specifying place of the commit change\n" \
+                "  + <subject> is a very short description of the change, in the \n" \
+                "    following format:\n" \
+                "        * imperative, present tense: “change” not “changed” / “changes”\n" \
+                "        * no capitalized first letter\n" \
+                "        * no dot (.) at the end" % (real_lineno,)
+    if lineno == 1:
+        if line:
+            return "E%d: Second line should be a blank line" % (real_lineno,real_lineno)
+    if lineno == 2:
+        if line.startswith('#') or not len(line) > 0:
+            return "E%d: Third line should be BODY.\n" \
+                "   The BODY should include the motivation for the change and contrast this\n" \
+                "   with previous behavior, and must be phrased in imperative present tense" % (real_lineno,)
+    if lineno == 3:
+        if line:
+            return "E%d: Line no %d should be a blank line" % (real_lineno,real_lineno)
+    if lineno == 4:
+        if line.startswith('#') or not len(line) > 0:
+            return "E%d: Last line is FOOTER, it should contain references.\n" \
+                "  The FOOTER should contain any information about Breaking Changes and is" 
+                "  also the place to reference GitHub issues that this commit closes" % (real_lineno,)
+    if not line.startswith('#'):
+        if len(line) > 72:
+            return "E%d: No line should be over 72 characters long." % (real_lineno,)
+
+    return False
 
 def read_commit_message():
     commit_msg = list()
@@ -18,7 +74,10 @@ def read_commit_message():
             if not empty_lines < 3:
                 errors.append("\n  %d blank lines\n" % (empty_lines) )
                 break
-            
+            commit_msg.append(line)
+            e = check_format_rules(lineno, stripped_line)
+            if e:
+                errors.append(e)
         return [commit_msg, errors]
 
 # Print commit erros on console and write them in commit message file
